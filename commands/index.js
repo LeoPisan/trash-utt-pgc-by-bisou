d269dbaa18444cfaca5f9db8766fce8e894b9185
plassasseigne <paul.lassasseigne@gmail.com>
2024-12-12 04:59:49 +0100

    feat(icalendar): add icalendar command
    - update .gitignore for the icalendar folder

--- commands/index.js
+++ commands/index.js
@@ -1,9 +1,11 @@
 const readAllEdt = require("./readAllEdt");
 const getRoom = require("./getRoom");
 const availableRoom = require("./availableRoom");
+const icalendar = require("./icalendar");
 
 module.exports = (cli) => {
     readAllEdt(cli);
     getRoom(cli);
     availableRoom(cli);
+    icalendar(cli);
 };
