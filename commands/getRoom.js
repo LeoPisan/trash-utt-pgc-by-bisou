ddd76dc031e07b7485d8353cfac330ee33f0444a
plassasseigne <paul.lassasseigne@gmail.com>
2024-12-12 03:05:08 +0100

    refactor(getRoom): move checkRoom into getRoom
    - to comply with the specifications
    - use options instead of args

--- commands/getRoom.js
+++ commands/getRoom.js
@@ -1,34 +1,59 @@
-const { parseCruFilesInDirectory } = require("../utils/cruUtils");
+const { parseCruFilesInDirectory, getCruDirectoryForCourse} = require("../utils/cruUtils");
 
 function getRoom(cli) {
     cli
-    .command("getRoom", "Get available rooms for a given time slot.")
-    .argument("<hours>", "The time slot to check for available rooms.")
-    .action(({ args, logger }) => {
-        try {
-            const hours = args.hours;
-            const dataDir = "data";
-            const parser = parseCruFilesInDirectory(dataDir);
+    .command("getroom", "Gives the rooms associated with a course or a time slot.")
+    .option("--class <class>", "The course that we want to know the rooms.")
+    .option("--hours <hours>", "The time slot to check for available rooms.")
+    .action(({ options, logger }) => {
+        if (Object.keys(options).length === 2) {
+            return logger.error(`Please choose either a <class> or a <hours>.`);
+        } else if (options.class !== undefined) {
+            try {
+                const course = options.class.toUpperCase();
+                const dataDir = "data";
+                const directoryPath = getCruDirectoryForCourse(course, dataDir);
+                const parser = parseCruFilesInDirectory(directoryPath);
+                const edt = parser.parsedData.find((edt) => edt.name === course);
 
-            const hoursRegex = /^\d{1,2}:\d{2}-\d{1,2}:\d{2}$/;
-            if (!hoursRegex.test(hours)) {
-                return logger.error(
-                    "SRUPC_2_E1: Invalid time slot format. Expected format: HH:MM-HH:MM"
-                );
+                if (!edt) {
+                    return logger.info(`SRUPC_1_E1 : Course ${course} not found.`);
+                }
+
+                logger.info(`${course} informations :`);
+                edt.sessions.forEach((session) => {
+                    logger.info(`Room: ${session.room}, Capacity: ${session.capacity}`);
+                });
+            } catch (error) {
+                logger.error(error.message);
             }
+        } else if (options.hours !== undefined) {
+            try {
+                const hours = options.hours;
+                const dataDir = "data";
+                const parser = parseCruFilesInDirectory(dataDir);
+                const hoursRegex = /^\d{1,2}:\d{2}-\d{1,2}:\d{2}$/;
+                const availableRooms = parser.availableRooms(hours);
 
-            const availableRooms = parser.availableRooms(hours);
+                if (!hoursRegex.test(hours)) {
+                    return logger.error(
+                        "SRUPC_2_E1: Invalid time slot format. Expected format: HH:MM-HH:MM"
+                    );
+                }
 
-            if (availableRooms.length === 0) {
-                logger.info("No available rooms found for the given time slot.");
-            } else {
-                logger.info("Available rooms:");
-                availableRooms.forEach((room) => logger.info(room));
+                if (availableRooms.length === 0) {
+                    logger.info("No available rooms found for the given time slot.");
+                } else {
+                    logger.info("Available rooms:");
+                    availableRooms.forEach((room) => logger.info(room));
+                }
+            } catch (error) {
+                logger.error(error.message);
             }
-        } catch (error) {
-            logger.error(error.message);
+        } else if (Object.keys(options).length <= 1) {
+            return logger.error(`Missing required argument. Please choose either a <class> or a <hours>.`);
         }
-    });
+    })
 }
 
 module.exports = getRoom;
--- commands/index.js
+++ commands/index.js
@@ -1,11 +1,9 @@
 const readAllEdt = require("./readAllEdt");
 const getRoom = require("./getRoom");
-const checkRoom = require("./checkRoom");
 const availableRoom = require("./availableRoom");
 
 module.exports = (cli) => {
     readAllEdt(cli);
     getRoom(cli);
-    checkRoom(cli);
     availableRoom(cli);
 };
