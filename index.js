1d098156ac6bd1a55871cc3c59c992acc3d84111
Paul Lassasseigne <paul@digital-cover.fr>
2024-12-15 13:35:40 +0100

    feat(index): add name and description for the cli

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const cli = require("@caporal/core").default;
 const commandsLoader = require("./commands");
 
 cli
-.version("cru-parser-cli")
-.version("0.1")
+  .name("Système de Planification Générale des Cours")
+  .version("0.1")
+  .description("Logiciel de planification générale des cours pour l'université centrale de la république de Sealand.")
 ;
 
 commandsLoader(cli);
